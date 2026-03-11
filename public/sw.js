e57a16dabf0276da56190e5cbc57fabc7bc13be0
connor <closcon000@connors-MacBook-Pro.local>
2026-03-10 20:45:50 -0400

    fix: patch navigator.serviceWorker before bundle loads in SW

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,22 @@
-// Nova Service Worker
+try {
+  if (typeof navigator !== 'undefined') {
+    Object.defineProperty(navigator, 'serviceWorker', {
+      get: () => undefined,
+      configurable: true
+    });
+  }
+} catch(e) {}
+
+if (typeof self.SharedWorker === 'undefined') self.SharedWorker = function(){};
+if (typeof self.localStorage === 'undefined') self.localStorage = {};
+
 importScripts('/scramjet/scramjet.bundle.js');
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
-
 const sw = new ScramjetServiceWorker();
 
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-
-self.addEventListener('fetch', (e) => {
-  if (sw.route(e)) {
-    e.respondWith(sw.fetch(e));
-  }
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => {
+  if (sw.route(e)) e.respondWith(sw.fetch(e));
 });
